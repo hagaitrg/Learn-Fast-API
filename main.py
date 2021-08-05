987d1029059ff1a1b3ff6675fca9aea38dd4641d
hagaitrg <disahagait@gmail.com>
2021-08-05 10:41:22 +0700

    learn endpoint parameters

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def hello():
-    return{"message" : "hello world"}
+grades = {
+    1: {
+        "name" : "dimas",
+        "class" : "SI-42-08",
+        "nim" : "1202184323",
+        "grade" : "90"
+    }
+}
+
+@app.get("/get-item/{item_id}")
+def get_data(item_id: int):
+    return grades[item_id]
